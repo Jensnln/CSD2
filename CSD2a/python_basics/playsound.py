55350d494619292f2a2410b4b4141f2fc314a194
Jensnln <jens.loeven@student.hku.nl>
2022-09-09 13:33:10 +0200

    Test Commit

--- CSD2a/python_basics/playsound.py
+++ CSD2a/python_basics/playsound.py
@@ -13,3 +13,6 @@ def loop_sample():
 #  For loop om de functie te loopen met als variabele de input die wordt gevraagd.
 for x in range(int(times_playing)):
     loop_sample()
+    
+
+    aanpassing
