28e0337463948122a24a2dd7300d4c1df25ccc75
Jens Loeven <jens.loeven@student.hku.nl>
2022-09-11 20:32:10 +0200

    Added VCS Shortcut script

--- CSD2a/python_basics/playsound.py
+++ CSD2a/python_basics/playsound.py
@@ -3,8 +3,7 @@ times_playing = input("Hoe vaak moet de sample worden afgespeeld? " )
 
 # Binnenhalen van de library en sample laden in de variabele "wave_obj".
 import simpleaudio as sa
-wave_obj = sa.WaveObject.from_wave_file("/Users/jens/Splice/sounds/packs/LOUD DRUMS - VOL. 1/Origin_Sound_-_LOUD_DRUMS_VOL_1/Drum_Hits/Cymbals/OS_LD1_HiHat_Dribble.wav")
-
+wave_obj = sa.WaveObject.from_wave_file("/home/jensnln/Documents/Sounds/506453__kbeezy88__220a-ch.wav")
 # Functie om de sample af te spelen.
 def loop_sample():
     play_obj = wave_obj.play()
