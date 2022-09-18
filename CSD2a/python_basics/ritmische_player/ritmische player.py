fad98f5635b4a9008f0d2f26639aecdd821850d6
Jensnln <jens.loeven@student.hku.nl>
2022-09-18 22:18:55 +0200

    Fixed time change

--- CSD2a/python_basics/ritmische_player/ritmische player.py
+++ CSD2a/python_basics/ritmische_player/ritmische player.py
@@ -1,4 +1,5 @@
 # Import all modules
+from sqlite3 import Timestamp
 import time
 
 # Declare variables
@@ -41,19 +42,28 @@ list_notes = input_file_list
 list_notes.pop()
 list_notes.pop(0)
 
-# Start a for loop as long as the list length.
-for notes in range(len(list_notes)):
-    # Indexes the list so it cycles through.
-    list_note = list_notes[notes]
 
-    # Call the function.
-    loop_sample()
+# Bool to check if the loop has ended
+has_more_notes : bool = True
 
-    # Calculate the noteLength 1sec / (BPM / 60) = amount of beat per second.
-    # BPS * the notelength of the notes.
-    noteLength = ((1/(int(noteTempo)/60))) * float(list_notes[notes])
-    time.sleep(noteLength)
+# Make a value to check what note you're on
+next_note = 0
 
+# Set the next note start time equal to the start time of the program.
+next_note_start_time = start_time
 
+# Make a while loop for as long as the boolean is true
+while has_more_notes:
+    current_time = time.time()
 
+    # If the start time for the next note is equal or smaller then the current time play the note.
+    if next_note_start_time  <= current_time:
 
+        # Calculate the length of the note
+        next_note_start_time = current_time + ((1/(int(noteTempo)/60))) * float(list_notes[next_note])
+        next_note += 1
+        loop_sample()
+
+        # If the next note index is higher then te current note turn the boolean false.
+        if next_note == len(list_notes):
+            has_more_notes = False
