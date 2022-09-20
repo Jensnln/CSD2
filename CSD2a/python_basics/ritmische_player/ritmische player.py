b37de380d82efaa845cdd7a22e03004f42cb734b
Jensnln <jens.loeven@student.hku.nl>
2022-09-20 15:03:34 +0200

    Cleanup

--- CSD2a/python_basics/ritmische_player/ritmische player.py
+++ CSD2a/python_basics/ritmische_player/ritmische player.py
@@ -4,21 +4,20 @@ import time
 # Declare variables
 start_time = time.time()
 played_time = 0
-prev_noteLength = 0
 
 
 # Open the input.txt file
-input_file = open('CSD2a/python_basics/ritmische_player/input.txt')
-input_file_content = input_file.read()
+input_list = open('CSD2a/python_basics/ritmische_player/input.txt')
+input_file_content = input_list.read()
 
 # Put the content in the list.
-input_file_list = input_file_content.split()
+input_list = input_file_content.split()
 
 # Lees de waardes uit en stop deze in een variable.
-input_file.close()
+input_list.close()
 
 # Input vragen hoevaak de sample af te spelen.
-numPlaybackTimes = input_file_list[0]
+numPlaybackTimes = input_list[0]
 
 
 
@@ -31,11 +30,11 @@ def loop_sample():
     # play_obj.wait_done()
 
 # Calculate how many hits a second.
-noteTempo = input_file_list[-1]
+noteTempo = input_list[-1]
 print(noteTempo)
 
 # Make a new list you can edit seperately.
-list_notes = input_file_list
+list_notes = input_list
 
 # Remove first and last index from list.
 list_notes.pop()
@@ -43,27 +42,27 @@ list_notes.pop(0)
 
 
 # Bool to check if the loop has ended
-has_more_notes : bool = True
+playing : bool = True
 
 # Make a value to check what note you're on
-next_note = 0
+note_counter = 0
 
 # Set the next note start time equal to the start time of the program.
 next_note_start_time = start_time
 
 # Make a while loop for as long as the boolean is true
-while has_more_notes:
-    current_time = time.time()
+while playing:
+    t = time.time()
 
     # If the start time for the next note is equal or smaller then the current time play the note.
-    if next_note_start_time  <= current_time:
+    if next_note_start_time  <= t:
 
         # Calculate the length of the note
-        next_note_start_time = current_time + ((1/(int(noteTempo)/60))) * float(list_notes[next_note])
-        next_note += 1
+        next_note_start_time = t + ((1/(int(noteTempo)/60))) * float(list_notes[note_counter])
+        note_counter += 1
         loop_sample()
         time.sleep(0.001)
 
         # If the next note index is higher then te current note turn the boolean false.
-        if next_note == len(list_notes):
-            has_more_notes = False
+        if note_counter == len(list_notes):
+            playing = False
