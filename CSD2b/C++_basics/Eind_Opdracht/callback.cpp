6e02d92bf755999f3724f98284b7b52eacc97155
Jensnln <jens.loeven@student.hku.nl>
2022-12-06 16:17:47 +0100

    Big commit

---
+++ CSD2b/C++_basics/Eind_Opdracht/callback.cpp
@@ -0,0 +1,25 @@
+#include "callback.h"
+
+
+void CustomCallback::prepare(int rate) {
+    samplerate = (float) rate;
+    sine.setSamplerate(samplerate);
+    square.setSamplerate(samplerate);
+    std::cout << "\nsamplerate: " << samplerate << "\n";
+}
+
+void CustomCallback::process(AudioBuffer buffer) {
+  for (int i = 0; i < buffer.numFrames; ++i) {
+    // write sample to buffer at channel 0, amp = 0.25
+    buffer.outputChannels[0][i] = 
+    // sine.getSample() + 
+    // square.getSample() +
+    saw.getSample() +
+    saw2.getSample();
+    // sine.tick();
+    // square.tick();
+    saw.tick();
+    saw2.tick();
+    saw2.phaseCancel();
+  }
+}
